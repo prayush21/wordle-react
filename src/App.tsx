665485c378f7bc93ca456fb57aef57e458b5e338
prayush21 <prayushdave@gmail.com>
2025-03-25 18:42:04 -0400

    Update Icons and fix keyboard issues

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import Board from "./components/Board";
 import Alert from "./components/Alert";
 import targetWords from "./components/targetWords";
 import dictionary from "./components/dictionary";
-import { FiSettings } from "react-icons/fi";
-import { FaHamburger } from "react-icons/fa";
-import { keyboard } from "@testing-library/user-event/dist/keyboard";
-import { stat } from "fs";
+import menuIcon from "./assets/menu_icon.svg"; // Import the SVG file
+import settingsIcon from "./assets/settings_icon.svg"; // Import the SVG file
+import helpIcon from "./assets/help.svg"; // Import the SVG file
+// import lightbulbIcon from "./assets/lightbulb_2.svg"; // Import the SVG file
+// import statsIcon from "./assets/equalizer.svg"; // Import the SVG file
 
 export interface TileObj {
   value: string;
@@ -30,7 +31,7 @@ export interface AppContextInterface {
   wordOfTheDay: string;
   keysState: KeyObj[];
   setKeysState: any;
-  keyClick: any,
+  keyClick: any;
 }
 
 export interface CurrentAttemptInterface {
@@ -54,7 +55,7 @@ const getTodaysWord = () => {
 };
 
 function App() {
-  const wordOfTheDay = 'onset';
+  const wordOfTheDay = "onset";
 
   const eTile: TileObj = {
     value: "",
@@ -128,124 +129,135 @@ function App() {
   };
 
   const isWordInDictionary = (Word: string) => {
-    return dictionary.find(word => word === Word) === undefined ? false : true;
-  }
+    return dictionary.find((word) => word === Word) === undefined
+      ? false
+      : true;
+  };
 
   const onEnterClick = (attempt: number, letterPos: number) => {
-    if(board !== undefined){
-    if (letterPos !== 5) {
-      
-      alertList && setAlertList([`Not Enough Letters`]);
-      return;
-    } else {
-      const word = board[attempt].map(({ value }) => value).join("").toLowerCase();
-      if(isWordInDictionary(word) && wordOfTheDay !== undefined){
-        if(word === wordOfTheDay){
-          const newBoard = [...board];
-          let newRow = [...board[attempt]].map((tile: TileObj): TileObj => {
-            return {...tile, state: "correct"};
-          });
-          newBoard[attempt] = newRow;
-          setBoard(newBoard);
-          alertList && setAlertList([`Thats Right!`]);
-        } else if(attempt === 5){
-          alertList && setAlertList([`Game Over`]);
-        } else {
-          const newBoard = [...board];
-          const guessArray = newBoard[attempt].map(element => {return {key: element.value.toLowerCase(), state: 'grey'};});
-          const solutionArray: any[] = [];
-          for(let index = 0; index < wordOfTheDay.length; index++){
-            solutionArray.push(wordOfTheDay[index]);
-            newBoard[attempt][index].state = "wrong";
-          }
-          console.log("guessAr, solAr", guessArray, solutionArray);
-          
-          // for (let index = 0; index < wordOfTheDay.length; index++) {
-          //   if(word[index] === wordOfTheDay[index]){
-          //     newBoard[attempt][index].state = "correct";
-          //     console.log("word[index]", word[index], newBoard[attempt][index].state);
-          //   } else if(wordOfTheDay.includes(word[index])){
-          //     newBoard[attempt][index].state = 'wrong-position'
-          //   } else {
-          //     newBoard[attempt][index].state = "wrong";
-          //   }
-          // }
-
-          guessArray.forEach((letter, index) => {
-            if(solutionArray[index] === letter.key){
-              console.log("correct", letter.key);
-              newBoard[attempt][index].state = "correct";
-              letter.state = "green";              
-              solutionArray[index] = null;
-            }
-          })
-
-          guessArray.forEach((letter, index) => {
-            if(solutionArray.includes(letter.key) && letter.state !== 'green'){
-              console.log("yellow", letter.key);
-              newBoard[attempt][index].state = 'wrong-position';
-              solutionArray[solutionArray.indexOf(letter.key)] = null;
-            }
-          })
-
-
-          
-          if(keysState){
-            const newKeysState = [...keysState];
-            console.log("newKeysState", newKeysState);
-            
-            newBoard[attempt].map(({value, state}) => {
-              
-              const index = newKeysState.findIndex(({keyValue, }) => keyValue === value);
-              console.log("inside", value, state, index);
-              // console.log("newKeysState", newKeysState[index].state);
-              newKeysState[index].state = state === 'active' ? "" : state;
-              
-            })
-            setKeysState(newKeysState);
-          }
-
-          setBoard(newBoard);
-          setCurrentAttempt({
-            ...currentAttempt,
-            attempt: attempt + 1,
-            letterPos: 0,
-          });
-        }
+    if (board !== undefined) {
+      if (letterPos !== 5) {
+        alertList && setAlertList([`Not Enough Letters`]);
+        return;
       } else {
-        alertList && setAlertList([`Word not in dictionary`]);
+        const word = board[attempt]
+          .map(({ value }) => value)
+          .join("")
+          .toLowerCase();
+        if (isWordInDictionary(word) && wordOfTheDay !== undefined) {
+          if (word === wordOfTheDay) {
+            const newBoard = [...board];
+            let newRow = [...board[attempt]].map((tile: TileObj): TileObj => {
+              return { ...tile, state: "correct" };
+            });
+            newBoard[attempt] = newRow;
+            setBoard(newBoard);
+            alertList && setAlertList([`Thats Right!`]);
+          } else if (attempt === 5) {
+            alertList && setAlertList([`Game Over`]);
+          } else {
+            const newBoard = [...board];
+            const guessArray = newBoard[attempt].map((element) => {
+              return { key: element.value.toLowerCase(), state: "grey" };
+            });
+            const solutionArray: any[] = [];
+            for (let index = 0; index < wordOfTheDay.length; index++) {
+              solutionArray.push(wordOfTheDay[index]);
+              newBoard[attempt][index].state = "wrong";
+            }
+            console.log("guessAr, solAr", guessArray, solutionArray);
+
+            // for (let index = 0; index < wordOfTheDay.length; index++) {
+            //   if(word[index] === wordOfTheDay[index]){
+            //     newBoard[attempt][index].state = "correct";
+            //     console.log("word[index]", word[index], newBoard[attempt][index].state);
+            //   } else if(wordOfTheDay.includes(word[index])){
+            //     newBoard[attempt][index].state = 'wrong-position'
+            //   } else {
+            //     newBoard[attempt][index].state = "wrong";
+            //   }
+            // }
+
+            guessArray.forEach((letter, index) => {
+              if (solutionArray[index] === letter.key) {
+                console.log("correct", letter.key);
+                newBoard[attempt][index].state = "correct";
+                letter.state = "green";
+                solutionArray[index] = null;
+              }
+            });
+
+            guessArray.forEach((letter, index) => {
+              if (
+                solutionArray.includes(letter.key) &&
+                letter.state !== "green"
+              ) {
+                console.log("yellow", letter.key);
+                newBoard[attempt][index].state = "wrong-position";
+                solutionArray[solutionArray.indexOf(letter.key)] = null;
+              }
+            });
+
+            if (keysState) {
+              const newKeysState = [...keysState];
+              console.log("newKeysState", newKeysState);
+
+              newBoard[attempt].map(({ value, state }) => {
+                const index = newKeysState.findIndex(
+                  ({ keyValue }) => keyValue === value
+                );
+                console.log("inside", value, state, index);
+                // console.log("newKeysState", newKeysState[index].state);
+                newKeysState[index].state = state === "active" ? "" : state;
+              });
+              setKeysState(newKeysState);
+            }
+
+            setBoard(newBoard);
+            setCurrentAttempt({
+              ...currentAttempt,
+              attempt: attempt + 1,
+              letterPos: 0,
+            });
+          }
+        } else {
+          alertList && setAlertList([`Word not in dictionary`]);
+        }
       }
     }
-  }
-}
+  };
 
-  const keyBoardKeyClick = ({key}: any) => {
+  const keyBoardKeyClick = ({ key }: any) => {
     console.log("key", key);
-    const keyValue = key === 'Backspace' ? 'Delete' : key;
-    if(keyValue !== 'Enter' && keyValue !== 'Delete' && keyValue.length > 1)  return;
-    if(keyValue === ' ')  return;
+    const keyValue = key === "Backspace" ? "Delete" : key;
+    if (keyValue !== "Enter" && keyValue !== "Delete" && keyValue.length > 1)
+      return;
+    if (keyValue === " ") return;
     keyClick(keyValue.toUpperCase());
-  }
+  };
 
   useEffect(() => {
     setBoard(mat);
   }, []);
 
   useEffect(() => {
-    window.addEventListener("keyup", keyBoardKeyClick)
+    window.addEventListener("keyup", keyBoardKeyClick);
 
-    return () => window.removeEventListener('keyup', keyBoardKeyClick);
-  }, [keyBoardKeyClick])
+    return () => window.removeEventListener("keyup", keyBoardKeyClick);
+  }, [keyBoardKeyClick]);
 
   return (
     <div className="App">
       <header className="App-header-module">
         <div className="header-left">
-          <FaHamburger />
+          <img src={menuIcon} alt="Menu Icon" className="menu-icon" />
         </div>
         <div className="title">Wordle</div>
         <div className="header-right">
-          <FiSettings />
+          <img src={settingsIcon} alt="Menu Icon" className="menu-icon" />
+          {/* <img src={lightbulbIcon} alt="Menu Icon" className="menu-icon" />
+          <img src={statsIcon} alt="Menu Icon" className="menu-icon" /> */}
+          <img src={helpIcon} alt="Menu Icon" className="menu-icon" />
         </div>
       </header>
       <AppContext.Provider
@@ -272,7 +284,7 @@ function App() {
         </div>
         <div className="alert-container">
           {alertList.map((value: string, index) => {
-            return <Alert key={index+value} data={value} />;
+            return <Alert key={index + value} data={value} />;
           })}
         </div>
       </AppContext.Provider>
--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -1,46 +1,26 @@
-import { useContext, useEffect, useRef, useState } from 'react';
-import { AppContext } from '../App';
-import './../styles/keyboard.css'
-import Key from './Key';
-
+import { useContext, useEffect, useRef, useState } from "react";
+import { AppContext } from "../App";
+import "./../styles/keyboard.css";
+import Key from "./Key";
 
 const Keyboard = () => {
   const ref = useRef<HTMLDivElement>(null);
   const appContext = useContext(AppContext);
-  const {
-    board,
-    setBoard,
-    currentAttempt,
-    setCurrentAttempt,
-    alertList,
-    setAlertList,
-    wordOfTheDay,
-    keysState,
-  } = appContext || {};
- 
-  
-  // useEffect(() => {
-  //   const handleClick = (e:MouseEvent) => {
-  //     console.log("clicked", e.target); 
-  //   }
-  //   const {current: element} = ref;
-
-  //   element?.addEventListener('click', handleClick);
-
-  //   return () => {
-  //     element?.removeEventListener('click', handleClick);
-  //   };
-  // },[])
-
+  const { keysState } = appContext || {};
 
   return (
     <>
       <div ref={ref} className="keyboard">
-        {keysState?.map(({keyValue, state}) => {
-          if(keyValue == 'A'){
-            return <><div className="space"></div><Key keyValue={keyValue} state={state}/></>
+        {keysState?.map(({ keyValue, state }) => {
+          if (keyValue == "A") {
+            return (
+              <>
+                <div className="space"></div>
+                <Key keyValue={keyValue} state={state} />
+              </>
+            );
           }
-        return <Key keyValue={keyValue} state={state}/>
+          return <Key keyValue={keyValue} state={state} />;
         })}
       </div>
     </>
@@ -48,106 +28,3 @@ const Keyboard = () => {
 };
 
 export default Keyboard;
-
-/*
-  <div ref={ref} className="keyboard">
-          <button className="key" data-key="Q">
-            Q
-          </button>
-          <button className="key" data-key="W">
-            W
-          </button>
-          <button className="key" data-key="E">
-            E
-          </button>
-          <button className="key" data-key="R">
-            R
-          </button>
-          <button className="key" data-key="T">
-            T
-          </button>
-          <button className="key" data-key="Y">
-            Y
-          </button>
-          <button className="key" data-key="U">
-            U
-          </button>
-          <button className="key" data-key="I">
-            I
-          </button>
-          <button className="key" data-key="O">
-            O
-          </button>
-          <button className="key" data-key="P">
-            P
-          </button>
-            <div className="space"></div>
-          <button className="key wrong-location" data-key="A">
-            A
-          </button>
-          <button className="key" data-key="S">
-            S
-          </button>
-          <button className="key" data-key="D">
-            D
-          </button>
-          <button className="key right" data-key="F">
-            F
-          </button>
-          <button className="key" data-key="G">
-            G
-          </button>
-          <button className="key" data-key="H">
-            H
-          </button>
-          <button className="key" data-key="J">
-            J
-          </button>
-          <button className="key wrong" data-key="K">
-            K
-          </button>
-          <button className="key" data-key="L">
-            L
-          </button>
-          <div className="space"></div>
-        
-          <button className="key large" data-enter>
-            Enter
-          </button>
-          <button className="key" data-key="Z">
-            Z
-          </button>
-          <button className="key" data-key="X">
-            X
-          </button>
-          <button className="key" data-key="C">
-            C
-          </button>
-          <button className="key" data-key="V">
-            V
-          </button>
-          <button className="key" data-key="B">
-            B
-          </button>
-          <button className="key" data-key="N">
-            N
-          </button>
-          <button className="key" data-key="M">
-            M
-          </button>
-          <button className="key large" data-delete>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24"
-              viewBox="0 0 24 24"
-              width="24"
-            >
-              <path
-                fill="var(--color-tone-1)"
-                d="M22 3H7c-.69 0-1.23.35-1.59.88L0 12l5.41 8.11c.36.53.9.89 1.59.89h15c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7.07L2.4 12l4.66-7H22v14zm-11.59-2L14 13.41 17.59 17 19 15.59 15.41 12 19 8.41 17.59 7 14 10.59 10.41 7 9 8.41 12.59 12 9 15.59z"
-              ></path>
-            </svg>
-          </button>
-      </div>
-
-*/
